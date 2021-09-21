5178536e7da6e55a7b8df75cea8d8ff181135398
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-09-22 01:23:37 +0200

    first components

---
+++ src/data/users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'Jacek Placek',
+    attendance: '52%',
+    average: '4.1',
+  },
+  {
+    name: 'Niewiem Kto',
+    attendance: '32%',
+    average: '3.2',
+  },
+  {
+    name: 'Jan Nepomucen',
+    attendance: '70%',
+    average: '4.6',
+  },
+  {
+    name: 'Wojciech Nieszczęsny',
+    attendance: '66%',
+    average: '4.3',
+  },
+  {
+    name: 'Hugo Kołontaj',
+    attendance: '99%',
+    average: '5.0',
+  },
+];
