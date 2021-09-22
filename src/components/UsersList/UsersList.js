5c917ca0c37cfede141b83ece400bbc0694e5078
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-09-22 02:03:04 +0200

    start

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { users } from 'data/users';
 import UsersListItem from 'components/UsersListItem/UsersListItem';
+import styles from 'assets/GlobalStyles.module.scss';
 
 const UsersList = () => (
   <div>
+    <div className={styles.box} />
     <ul>
       {users.map((userData) => (
         <UsersListItem userData={userData} />
