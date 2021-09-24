7d5a1e5f359524ab57b9f4eb06377e7c2dc045b1
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-09-24 11:07:48 +0200

    forms in progress

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,37 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { users as usersData } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList } from './UsersList.styles';
-
-const mockAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (usersData) {
-        resolve([...usersData]);
-      } else {
-        reject({ message: 'Error' });
-      }
-    }, 2000);
-  });
-};
+import FormField from 'components/molecules/FormField/FormField';
+import { StyledTitle } from './UsersList.styles';
+import { Button } from 'components/atoms/Button/Button';
 
 const UsersList = () => {
   const [users, setUsers] = useState(usersData);
-  const [isLoading, setLoadingState] = useState([]);
+  const [formValues, setFormValues] = useState({
+    name: '',
+    attendance: '',
+    average: '',
+  });
 
   const deleteUser = (name) => {
     const filteredUsers = users.filter((user) => user.name !== name);
     setUsers(filteredUsers);
   };
 
+  const handleInputChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
   return (
-    <Wrapper>
-      <StyledList>
-        {users.map((userData) => (
-          <UsersListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
-        ))}
-      </StyledList>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <StyledTitle>Add new students</StyledTitle>
+        <FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputChange} />
+        <FormField label="Attendance" id="attendance" name="attendance" />
+        <FormField label="Average" id="average" name="average" />
+        <Button>Add</Button>
+      </Wrapper>
+      <Wrapper>
+        <StyledTitle>Students list</StyledTitle>
+        <StyledList>
+          {users.map((userData) => (
+            <UsersListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
+          ))}
+        </StyledList>
+      </Wrapper>
+    </>
   );
 };
 
