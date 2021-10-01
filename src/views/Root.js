679d233ebe7d3d0c4f9d640b28ae6f4afa951eed
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-10-01 18:53:53 +0200

    UsersProvider added (clr code)

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Dashboard from './Dashboard';
-import { users as usersData } from 'data/users';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
@@ -8,41 +7,15 @@ import { Wrapper } from './Root.style';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AddUser from './AddUser';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
-
-export const UsersContext = React.createContext({
-  users: [],
-  handleAddUser: () => {},
-  deleteUser: () => {},
-});
+import UsersProvider from 'providers/UsersProvider';
 
 const Root = () => {
-  const [users, setUsers] = useState(usersData);
-
-  const deleteUser = (name) => {
-    const filteredUsers = users.filter((user) => user.name !== name);
-    setUsers(filteredUsers);
-  };
-
-  const handleAddUser = (values) => {
-    const newUser = {
-      name: values.name,
-      attendance: values.attendance,
-      average: values.average,
-    };
-    setUsers([newUser, ...users]);
-  };
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <MainTemplate>
-          <UsersContext.Provider
-            value={{
-              users,
-              handleAddUser,
-              deleteUser,
-            }}
-          >
+          <UsersProvider>
             <Wrapper>
               <Switch>
                 <Route path="/add-user">
@@ -53,7 +26,7 @@ const Root = () => {
                 </Route>
               </Switch>
             </Wrapper>
-          </UsersContext.Provider>
+          </UsersProvider>
         </MainTemplate>
       </ThemeProvider>
     </Router>
