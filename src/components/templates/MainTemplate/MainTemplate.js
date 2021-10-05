1da63e54ca51a4908177fee19d14b0c99d9d2e00
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-10-06 00:10:51 +0200

    x

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Navigation from 'components/organisms/Navigation/Navigation';
 import { Wrapper } from './MainTemplate.styles';
+import styled from 'styled-components';
+
+const SearchBar = styled.div`
+  grid-row: 1/2;
+  grid-column: 2/3;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
+`;
+
+const News = styled.div`
+  grid-row: 1/2;
+  grid-column: 3/3;
+  border-left: 1px solid ${({ theme }) => theme.colors.grey};
+`;
 
 const MainTemplate = ({ children }) => {
   return (
     <Wrapper>
       <Navigation />
+      <SearchBar />
       {children}
+      <News>
+        <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+      </News>
     </Wrapper>
   );
 };
