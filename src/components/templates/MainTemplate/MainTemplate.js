4350b5cd3b690bed9f0aab7834fc5541feb0c582
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-10-07 14:24:35 +0200

    searchBar

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,13 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navigation from 'components/organisms/Navigation/Navigation';
 import { Wrapper } from './MainTemplate.styles';
-import styled from 'styled-components';
-
-const SearchBar = styled.div`
-  grid-row: 1/2;
-  grid-column: 2/3;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
-`;
+import SearchBar from 'components/organisms/SearchBar/SearchBar';
 
 const News = styled.div`
   grid-row: 1/2;
