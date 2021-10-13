8a50d9046fcbc2f999b0347cc4ddbea264fd0b8f
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-10-13 15:30:40 +0200

    NavGroups

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 import { Link, useParams } from 'react-router-dom';
 import UsersList from 'components/organisms/UsersList/UsersList';
+import styled from 'styled-components';
 
 const NavGroups = styled.nav``;
 
