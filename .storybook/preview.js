54a90c86f6478f34d57bd38b90c43300989e6fb7
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-10-24 22:48:23 +0200

    button story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'assets/styles/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
