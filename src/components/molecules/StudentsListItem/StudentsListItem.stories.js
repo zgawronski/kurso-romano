407386294a21053036d03e8b311103637f58a8ca
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-10-24 23:31:25 +0200

    StudentsListItem stories

--- src/components/molecules/StudentsListItem/StudentsListItem.stories.js
+++ src/components/molecules/StudentsListItem/StudentsListItem.stories.js
@@ -13,7 +13,38 @@ const mockedData = {
 
 const Template = (args) => <StudentsListItem {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-  userData: mockedData,
+export const MediumGrades = Template.bind({});
+MediumGrades.args = {
+  userData: {
+    name: 'Adam Romański',
+    attendance: '55%',
+    average: '3.5',
+  },
+};
+
+export const GoodGrades = Template.bind({});
+GoodGrades.args = {
+  userData: {
+    name: 'Adam Romański',
+    attendance: '100%',
+    average: '5.0',
+  },
+};
+
+export const BadGrades = Template.bind({});
+BadGrades.args = {
+  userData: {
+    name: 'Adam Romański',
+    attendance: '25%',
+    average: '2.0',
+  },
+};
+
+export const NoAverage = Template.bind({});
+NoAverage.args = {
+  userData: {
+    name: 'Adam Romański',
+    attendance: '75%',
+    average: null,
+  },
 };
